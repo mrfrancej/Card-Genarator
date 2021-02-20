3fe8b9b8fa807fc0534160dc7dc2eb352d83c9f8
mrfrancej <france.jeffrey@GMAIL.COM>
2021-02-20 00:42:18 +0000

    first commit

--- src/main.js
+++ src/main.js
@@ -6,5 +6,22 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  const values = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  const suits = ["Hearts", "Diamonds", "Spades", "Clubs"];
+
+  //   console.log("Hello Rigo from the console!");
 };
