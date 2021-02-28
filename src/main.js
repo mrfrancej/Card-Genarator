a9569fafee977f205e69d49c5a51cc408275354e
mrfrancej <france.jeffrey@GMAIL.COM>
2021-02-28 19:20:47 +0000

    updated the main page

--- src/main.js
+++ src/main.js
@@ -5,8 +5,8 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  const values = [
+window.onload = () =>{
+  const number = [
     "A",
     "2",
     "3",
@@ -21,7 +21,14 @@ window.onload = function() {
     "Q",
     "K"
   ];
-  const suits = ["Hearts", "Diamonds", "Spades", "Clubs"];
+  const suit = ["Hearts", "Diamonds", "Spades", "Clubs"];
 
-  //   console.log("Hello Rigo from the console!");
+  let randomnumberposition = Math.floor(Math.random() * number.length);
+  let randomsuitposition = Math.floor(Math.random() * suit.length);
+
+    let randomsuit = suit[randomsuitposition];
+
+  document.querySelector("suit").innerHTML = randomsuit;
+  document.querySelector("number").innerHTML = randomnumberposition;
+  document.querySelector("suit").innerHTML = randomsuit;
 };
